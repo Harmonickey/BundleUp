48d13ab44bf40b57d66c8dabbfd4d4fb9c8c8982
wfanzju <wfanzju@gmail.com>
2014-04-05 13:51:28 -0500

    add wind

--- js/bundleup.js
+++ js/bundleup.js
@@ -24,15 +24,33 @@ $(function(){
 		+state+"/"+city+".json",
 		dataType:"jsonp",
 		success: function(parsed_json) {
-			var p1 = parsed_json['forecast']['txt_forecast']['forecastday'][((time < 17) ? 0 : 1)]['pop'];
-			var p2 = parsed_json['forecast']['txt_forecast']['forecastday'][((time < 17) ? 0 : 1)]['icon_url'];
+			var precip = parsed_json['forecast']['txt_forecast']['forecastday'][((time < 17) ? 0 : 1)]['pop'];
+			var icon = parsed_json['forecast']['txt_forecast']['forecastday'][((time < 17) ? 0 : 1)]['icon_url'];
 		
-			$("#precip").append(p1);
-			$("#weather").attr("src", p2);
+			$("#precip").append(precip);
+			$("#weather").attr("src", icon);
 		},
 		error: function() {
 			$("#error").append("Problem with finding forecast.");
 			$("#error").prop("hidden", false);
 		}
+	});
+
+
+	$.ajax({
+		url:"http://api.wunderground.com/api/5bb4e5428ca66275/conditions/q/"
+		+state+"/"+city+".json",
+		dataType:"jsonp",
+		success: function(parsed_json) {
+			var wind_dir = parsed_json['current_observation']['wind_dir'];
+			var wind_mph = parsed_json['current_observation']['wind_mph'];
+			$("#wind_mph").append(wind_mph);
+			$("#wind_dir").append(wind_dir);
+		},
+		error: function() {
+			$("#error").append("Problem with finding wind conditions.");
+			$("#error").prop("hidden", false);
+		}
+
 	});
 });
